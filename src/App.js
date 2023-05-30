e60fe6ee475ee57ac5b74bf5566063e652dd7e35
eaglehero <kingeagle2040@gmail.com>
2023-05-31 00:21:41 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import Num1pro from "./Components.js/Num1pro";
+
+function App() {
+  return (
+    <div className="App">
+      <Num1pro />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components.js/Num1pro.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+function Num1pro(){
+    return (
+        <div>
+            <h2>hello mr mina</h2>
+        </div>
+    )
+}
+export default Num1pro;
