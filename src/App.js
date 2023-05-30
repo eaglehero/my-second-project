86c95d0717357bd1124deb1563bb890220a46f36
eaglehero <kingeagle2040@gmail.com>
2023-05-31 00:39:22 +0300

    adite

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from "react";
 import Num1pro from "./Components.js/Num1pro";
+import Num2pro from "./Components.js/Num2pro";
 
 function App() {
   return (
     <div className="App">
       <Num1pro />
+      <Num2pro />
     </div>
   );
 }
---
+++ src/Components.js/Num2pro.js
@@ -0,0 +1,10 @@
+import React, {Component} from "react";
+
+function Num2pro(){
+    return (
+        <div>
+            <h1>welcome mina to the new second project</h1>
+        </div>
+    )
+}
+export default Num2pro;
